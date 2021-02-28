96b26481d80c9976d0ca78af6d6786d89da4c259
Josie Huddleston <79789329+StrangeButUntrue@users.noreply.github.com>
2021-02-28 23:42:40 +0000

    Some useful bits from the faffcommits

--- docs/conf.py
+++ docs/conf.py
@@ -65,8 +65,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'MicroPython'
-copyright = '2014-2019, Damien P. George, Paul Sokolovsky, and contributors'
+project = 'MicroPython Lego RI5'
+copyright = '2014-2021, Damien P. George, Paul Sokolovsky, StrangeButUntrue and contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -235,8 +235,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'MicroPython.tex', 'MicroPython Documentation',
-   'Damien P. George, Paul Sokolovsky, and contributors', 'manual'),
+  (master_doc, 'MicroPython.tex', 'MicroPython Lego RI5 Documentation',
+   'Damien P. George, Paul Sokolovsky, StrangeButUntrue and contributors', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -265,8 +265,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'micropython', 'MicroPython Documentation',
-     ['Damien P. George, Paul Sokolovsky, and contributors'], 1),
+    ('index', 'micropythonri5', 'MicroPython Lego RI5 Documentation',
+     ['Damien P. George, Paul Sokolovsky, StrangeButUntrue and contributors'], 1),
 ]
 
 # If true, show URL addresses after external links.
@@ -279,8 +279,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'MicroPython', 'MicroPython Documentation',
-   'Damien P. George, Paul Sokolovsky, and contributors', 'MicroPython', 'One line description of project.',
+  (master_doc, 'MicroPythonRI5', 'MicroPython Lego RI5 Documentation',
+   'Damien P. George, Paul Sokolovsky, StrangeButUntrue and contributors', 'MicroPythonRI5', 'One line description of project.',
    'Miscellaneous'),
 ]
 
